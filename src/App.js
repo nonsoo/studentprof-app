d52b2290c17dd6f84b842a4928c5d0954e1bcdc3
Geogre <nonsootoh@gmail.com>
2021-05-12 16:33:25 -0400

    Loaded user Info

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import "./Css/layout.css";
 import "./Css/Comp.css";
 
 import Profiles from "./Components/Profiles";
 
 function App() {
+  const [userProf, setUserProf] = useState();
+
+  useEffect(() => {
+    axios.get(`https://api.hatchways.io/assessment/students`).then((res) => {
+      const responseProf = res.data;
+      setUserProf(responseProf);
+    });
+  }, []);
+
+  console.log(userProf);
   return (
     <div className="studentProf--appC">
-      <div className="ProfilesContainer">
-        <input
-          type="text"
-          className="SearchField"
-          placeholder="Search By Name"
-        />
-        <Profiles />
-        <Profiles />
-        <Profiles />
-      </div>
+      {userProf && (
+        <div className="ProfilesContainer">
+          <input
+            type="text"
+            className="SearchField"
+            placeholder="Search By Name"
+          />
+          {userProf.students.map((prof) => (
+            <div key={prof.id}>
+              <Profiles
+                fName={prof.firstName}
+                lName={prof.lastName}
+                Email={prof.email}
+                Company={prof.company}
+                Skill={prof.skill}
+                Img={prof.pic}
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/Components/Profiles.jsx
+++ src/Components/Profiles.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
-const Profiles = ({ Name, Email, Company, Average }) => {
+const Profiles = ({ fName, lName, Email, Company, Skill, Average, Img }) => {
   return (
     <div className="Profiles">
-      {/* <img src="" alt="" className="Profiles__Img" /> */}
-      <div className="Profiles__Img"></div>
+      <img src={`${Img}`} alt="" className="Profiles__Img" />
+      {/* <div className="Profiles__Img"></div> */}
       <div className="Profiles__TextCon">
-        <p className="Profiles__Title">{Name}</p>
+        <p className="Profiles__Title">
+          {fName} {lName}
+        </p>
         <p className="Profiles__Email subText">Email: {Email}</p>
         <p className="Profiles__Company subText">Company: {Company}</p>
+        <p className="Profiles__Company subText">Skill: {Skill}</p>
         <p className="Profiles__Average subText">Average: {Average}</p>
       </div>
     </div>
@@ -18,7 +21,8 @@ const Profiles = ({ Name, Email, Company, Average }) => {
 export default Profiles;
 
 Profiles.defaultProps = {
-  Name: "John Doe",
+  fName: "John",
+  lName: "Doe",
   Email: "Sample@sample.com",
   Company: "Sample Company",
   Average: "100%",
